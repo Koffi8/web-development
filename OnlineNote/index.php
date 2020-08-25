f23e10428313bc619b9a5e65a3a30c4d74f12690
Koffi Nyonouglo <nyonouglokoffi8@gmail.com>
2020-08-25 02:50:09 +0000

    Online Note In PHP
    Online note in php

---
+++ OnlineNote/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>OneLine Notes</title>
+     
+      
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+      
+    <!-- Bootstrap -->
+      
+      
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+     <link rel="stylesheet" href="style.css">  
+   
+      
+      
+      
+
+   
+  </head>
+  <body>
+   
+
+      
+      
+     <!--navigation bar-->
+      <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
+       <div class="container-fluid">
+          <div class="navbar-header">
+           <a class="navbar-brand">Online Notes</a>
+            <button type="button" class="navbar-toggle" data-target="#navbarCollapse" data-toggle="collapse">
+            <span class="sr-only">Toggle navigation</span>
+             <span class="icon-bar"></span> 
+             <span class="icon-bar"></span> 
+             <span class="icon-bar"></span> 
+              
+            </button>
+           
+          </div>
+           <div class="navbar-collapse collapse" id="navbarCollapse">
+               <ul class="nav navbar-nav">
+               <li class="active"><a href="#">Home</a></li>
+               <li><a href="#">Help</a></li>
+               <li><a href="#">Contact</a></li>
+               
+               </ul>
+               
+               <ul class="nav navbar-nav navbar-right">
+               <li><a href="#">Login</a></li>
+               
+               </ul>
+           
+           </div>
+           
+       </div>
+      
+      </nav> 
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      <!-- Latest compiled and minified JavaScript -->
+ <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+  </body>
+</html>
